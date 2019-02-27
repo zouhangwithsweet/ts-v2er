777205316a928bda8b171c84472044d3519b02d4
zouhang <1102639563@qq.com>
2019-02-27 14:37:10 +0800

    :zap: new vue cli

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 const resolve = file => path.resolve(__dirname, file)
 
 module.exports = {
-  baseUrl: '/',
+  publicPath: '',
   css: {
     loaderOptions: {
       stylus: {
