9cc4a30a0918eb373c528b790ef046a76b0086b4
zouhang <1102639563@qq.com>
2019-04-25 17:10:21 +0800

    :zap: test 子路径

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 const resolve = file => path.resolve(__dirname, file)
 
 module.exports = {
-  publicPath: '/',
+  publicPath: '/web',
   css: {
     loaderOptions: {
       stylus: {
